1ee0aad81b6e688d17cb05d1039f3d7cb6372a05
s4llm4n <s4llm4n@gmail.com>
2022-08-31 07:16:07 +0700

    commit migrate_table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,13 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            
+            $table->longText('address_one');
+            $table->longText('address_two');
+            $table->integer('provinces_id');
+            $table->integer('regencies_id');
+
+            $table->softDeletes();
             $table->rememberToken();
             $table->timestamps();
         });
