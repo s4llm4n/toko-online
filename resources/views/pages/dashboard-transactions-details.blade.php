c1a0b510b2020ff123e01f6049314fcfebab2adc
s4llm4n <s4llm4n@gmail.com>
2022-09-02 13:49:44 +0700

    tambah table

--- resources/views/pages/dashboard-transactions-details.blade.php
+++ resources/views/pages/dashboard-transactions-details.blade.php
@@ -42,7 +42,7 @@
                         <div class="product-subtitle">12 Januari, 2021</div>
                       </div>
                       <div class="col-12 col-md-6">
-                        <div class="product-title">Status</div>
+                        <div class="product-title">Payment Status</div>
                         <div class="product-subtitle text-danger">
                           Pending
                         </div>
@@ -89,14 +89,13 @@
                         <div class="product-subtitle">Indonesia</div>
                       </div>
                       <div class="col-12 col-md-3">
-                        <div class="product-title">Status</div>
+                        <div class="product-title"> Shipping Status</div>
                         <select 
                           name="status" 
                           id="status" 
                           class="form-control" 
                           v-model="status"
                         >
-                          <option value="UNPAID">Unpaid</option>
                           <option value="PENDING">Pending</option>
                           <option value="SHIPPING">Shipping</option>
                           <option value="SUCCESS">Success</option>
