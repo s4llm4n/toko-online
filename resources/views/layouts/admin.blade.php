ffa709a77e2cbe0c51394327e3d44a11133375b5
s4llm4n <s4llm4n@gmail.com>
2022-09-08 12:16:49 +0700

    commit prodgalleryrequest

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -33,9 +33,14 @@
             Dashboard
           </a>
             <a href="{{ route('product.index') }}"
-            class="list-group-item list-group-item-action {{ (request()->is('admin/product*')) ? 'active' : '' }}"
+            class="list-group-item list-group-item-action {{ (request()->is('admin/product')) ? 'active' : '' }}"
           > 
             Poducts 
+          </a>
+          <a href="{{ route('product-gallery.index') }}"
+            class="list-group-item list-group-item-action {{ (request()->is('admin/product-gallery*')) ? 'active' : '' }}"
+          > 
+            Galleries 
           </a>
           <a 
             href="{{ route('category.index') }}"
--- routes/web.php
+++ routes/web.php
@@ -50,6 +50,7 @@ Route::prefix('admin')
         Route::resource('category', 'CategoryController');
         Route::resource('user', 'UserController');
         Route::resource('product', 'ProductController');
+        Route::resource('product-gallery', 'ProductGalleryController');
     });
 
 Auth::routes();
